8ff5396295b39669685be822f5823de50ecd41cd
drodriguez <drodriguez@izzi.mx>
2024-07-23 14:28:51 -0600

    Mostrar de forma condicional el texto si es ejercicio o comida al guardar

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -19,6 +19,11 @@ export default function Form() {
     });
   }
 
+  const isValidActivity = () => {
+    const { name, calories } = activity
+    return name.trim() !== '' && calories > 0
+  }
+
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
@@ -66,8 +71,9 @@ export default function Form() {
 
       <input
         type="submit"
-        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-        value="Guardar comida o Guardar Ejercicio"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+        value={activity.category == 1 ? 'Guardar comida' : 'Guardar ejercicio'}
+        disabled={!isValidActivity()}
       />
     </form>
   );
