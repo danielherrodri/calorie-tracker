96ee708c9c4844ecd22e4bde429ff80560fca11d
drodriguez <drodriguez@izzi.mx>
2024-07-23 14:10:42 -0600

    Escribiendo en el state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,6 +8,13 @@ export default function Form() {
     calories: 0
   });
 
+  const handleChange = (e) => {
+    setActivity({
+      ...activity,
+      [e.target.id]: e.target.value
+    });
+  }
+
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-1 gap-3">
@@ -16,6 +23,7 @@ export default function Form() {
         </label>
         <select
           id="category" value={activity.category}
+          onChange={handleChange}
           className="border border-slate-300 p-2 rounded-lg w-full bg-white"
         >
           {categories.map((category) => (
@@ -33,6 +41,7 @@ export default function Form() {
           type="text"
           id="name"
           value={activity.name}
+          onChange={handleChange}
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
         />
@@ -45,6 +54,7 @@ export default function Form() {
           type="number"
           id="calories"
           value={activity.calories}
+          onChange={handleChange}
           className="border border-slate-300 p-2 rounded-lg"
           placeholder="Calorías. Ej. 300 0 500"
         />
