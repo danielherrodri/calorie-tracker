86ed29215e5c295a9c8e619119b85fc4be3bec2c
drodriguez <drodriguez@izzi.mx>
2024-07-24 15:29:38 -0600

    Creando un componente para mostrar el total de calorías

---
+++ src/components/CalorieTracker.tsx
@@ -0,0 +1,10 @@
+import { Activity } from "../types"
+
+type CalorieTrackerProps = {
+    activities: Activity[]
+}
+export default function CalorieTracker({ activities }: CalorieTrackerProps) {
+    return (
+        <div>CalorieTracker</div>
+    )
+}
