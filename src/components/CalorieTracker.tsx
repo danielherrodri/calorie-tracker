7e635d928aae90b535439bf5dd26a57728c375bc
danielherrodri <84465666+danielherrodri@users.noreply.github.com>
2024-07-26 23:10:09 -0600

    Mostrando el diferencial de Calorías

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,24 +1,43 @@
-import { useMemo } from "react"
-import { Activity } from "../types"
-import CalorieDisplay from "./CalorieDisplay"
+import { useMemo } from "react";
+import { Activity } from "../types";
+import CalorieDisplay from "./CalorieDisplay";
 
 type CalorieTrackerProps = {
-    activities: Activity[]
-}
+  activities: Activity[];
+};
 export default function CalorieTracker({ activities }: CalorieTrackerProps) {
-
-    const caloriesConsumed = useMemo(() => activities.reduce((total, activity) => activity.category === 1 ? total + activity.calories : total, 0), [activities])
-    const caloriesBurned = useMemo(() => activities.reduce((total, activity) => activity.category === 2 ? total + activity.calories : total, 0), [activities])
-
-    return (
-        <>
-            <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
-            <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-
-                <CalorieDisplay calories={caloriesConsumed} text={'Consumidas'} />
-                <CalorieDisplay calories={caloriesBurned} text={'Quemadas'} />
-
-            </div>
-        </>
-    )
+  const caloriesConsumed = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 1 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+  const caloriesBurned = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.category === 2 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+  const netCalories = useMemo(
+    () => caloriesConsumed - caloriesBurned,
+    [caloriesBurned, caloriesConsumed]
+  );
+  return (
+    <>
+      <h2 className="text-4xl font-black text-white text-center">
+        Resumen de Calorías
+      </h2>
+      <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+        <CalorieDisplay calories={caloriesConsumed} text={"Consumidas"} />
+        <CalorieDisplay calories={caloriesBurned} text={"Quemadas"} />
+        <CalorieDisplay calories={netCalories} text={"Diferencia"} />
+      </div>
+    </>
+  );
 }
